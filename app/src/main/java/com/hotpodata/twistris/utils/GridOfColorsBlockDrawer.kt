060c3ff422ab9700d9c53847427ae30a5175cf67
Joel Drotos <jrdrotos@gmail.com>
2016-01-09 09:02:18 -0600

    Apply blocklib updates to prevent hardware layer badness

--- app/src/main/java/com/hotpodata/twistris/utils/GridOfColorsBlockDrawer.kt
+++ app/src/main/java/com/hotpodata/twistris/utils/GridOfColorsBlockDrawer.kt
@@ -1,17 +1,19 @@
 package com.hotpodata.twistris.utils
 
 import android.graphics.Canvas
+import android.graphics.RectF
 import com.hotpodata.blocklib.view.GridBinderView
 
 /**
  * Created by jdrotos on 1/2/16.
  */
 object GridOfColorsBlockDrawer : GridBinderView.RedBlockDrawer() {
-    override fun drawBlock(canvas: Canvas, data: Any) {
+    override fun drawBlock(canvas: Canvas, blockCoords: RectF, data: Any) {
         if (data is Int) {
-            canvas.drawColor(data)
+            paint.color = data
+            canvas.drawRect(blockCoords, paint)
         } else {
-            super.drawBlock(canvas, data)
+            super.drawBlock(canvas, blockCoords, data)
         }
     }
 }
